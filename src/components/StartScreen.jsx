ffc21a2372d73997918ed8542a84fa442e4f3649
YassaEmad <yassaemad86@gmail.com>
2025-12-30 01:21:16 +0200

    Update StartScreen.jsx

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -9,8 +9,6 @@ function StartScreen({ dispatch }) {
   const dropdownRef = useRef(null);
 
   const options = [
-    { value: "db1", label: "CH (11,12) GEMINI" },
-    { value: "db2", label: "CH (11,12) CHATGPT" },
     { value: "db3", label: "CH (11,12) NOTEBOOK LM" },
     { value: "db4", label: "NOTEBOOK LM EXAM" },
     { value: "db5", label: "NOTEBOOK LM EXAM-2" },
